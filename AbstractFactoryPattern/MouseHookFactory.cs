9f93192372fd948affce3f3587ccee299cd299b9
Artemissio <artemissterio@gmail.com>
2020-04-05 15:39:29 +0300

    Lab4. Bridge Pattern

---
+++ AbstractFactoryPattern/MouseHookFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemActivityMonitor.Models;
+
+namespace SystemActivityMonitor.AbstractFactoryPattern
+{
+    public class MouseHookFactory : IFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new MouseHook();
+        }
+    }
+}
---
+++ BridgePattern/IPerformanceSingleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemActivityMonitor.Models;
+
+namespace SystemActivityMonitor.BridgePattern
+{
+    public interface IPerformanceSingleton
+    {
+        List<Performance> GetPerformance();
+    }
+}
